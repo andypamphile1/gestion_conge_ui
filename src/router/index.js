bcde5abc2023eb76b50a34006a7a845dece62f38
kagisye andy pamphile <kagisyeandypamphile@gmail.com>
2024-01-25 12:37:32 +0200

    hello

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from "../views/LoginView.vue"
-
+import Listeconge from "../view/Listeconge.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
    {
-    path:"/login",
-    name:"loginview",
+    path:"/LoginView",
+    name:"LoginView",
     component:LoginView
+   },
+   {
+    path:"/Listeconge",
+    name:"Listeconge", 
+    component:Listeconge
    }
   ]
 })
---
+++ src/views/Listeconger.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+wassgud
+    </div>
+</template>
